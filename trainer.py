77c158af99987e9c42a4a2a4a0a5a8b14aee059f
amireza1422 <amireza1422@gmail.com>
2024-07-22 09:01:12 +0330

    Detailing function
    This function provides an statistic of each patient data characteristics

--- trainer.py
+++ trainer.py
@@ -18,6 +18,7 @@ class Trainer:
     def __init__(self, chosen_shapes:List[Tuple], chosen_protocols:List[str]) -> None:
         self.data_path = os.path.join(os.getcwd(), "data", "main", "iaaa-mri-challenge")
         self.train_csv = self._get_train_csv()
+        self.detail_dict = self.detailing(self.train_csv)
 
     def _get_train_csv(self) -> pd.DataFrame:
         """
@@ -27,3 +28,39 @@ class Trainer:
         train_csv_path = os.path.join(self.data_path, "train.csv")
         train_csv = pd.read_csv(train_csv_path)
         return train_csv
+    def detailing(self, data:pd.DataFrame) -> dict:
+        """
+        Creates a dictionary of patient ids on keys and values of another dictionary carrying some detail
+
+        data: data with patients id
+        """
+
+        detail_dict = {}
+        patients = data["SeriesInstanceUID"]
+        for patient in patients:
+            # label
+            prediction = data[data["SeriesInstanceUID"] == patient]["prediction"]
+
+            # image shape & protocol
+            patient_path = os.path.join(self.data_path, "data", patient)
+            shape, protocol = self._get_image_sp(patient_path)
+
+            # slice count
+            slices = os.listdir(patient_path)
+            try:
+                slices.remove(".DS_Store")
+            except:
+                pass
+
+            slice_count = len(slices)
+
+            # inserting details with key of patient id
+            detail_dict[patient] = {
+                "label": prediction,
+                "shape": shape,
+                "protocol": protocol,
+                "slice_count": slice_count
+            }
+
+        return detail_dict
+
