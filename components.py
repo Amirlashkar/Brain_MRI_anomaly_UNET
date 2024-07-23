20e42db0d8fdf71392436bbf57d5b39e247d740f
amireza1422 <amireza1422@gmail.com>
2024-07-23 18:29:11 +0330

    UNet revision
    Optimizer couldn't get model parameters cause layers should be defined at init function

--- components.py
+++ components.py
@@ -120,53 +120,80 @@ class AutoEncoder(nn.Module):
 
 
 class UNet(nn.Module):
-    def conv_block(self, input:torch.Tensor, in_channels:int, out_channels:int):
-        x = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)(input)
-        x = nn.BatchNorm2d(out_channels)(x)
-        x = nn.ReLU(inplace=True)(x)
-        x = nn.Dropout2d(.25)(x)
-        x = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)(x)
-        x = nn.BatchNorm2d(out_channels)(x)
-        x = nn.ReLU(inplace=True)(x)
-        x = nn.Dropout2d(.25)(x)
+    """
+    More reliable AutoEncoder
+    """
 
-        return x
+    def __init__(self):
+        super(UNet, self).__init__()
 
-    def upconv(self, input:torch.Tensor, in_channels:int, out_channels:int):
-        up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)(input)
-        return up
+        self.enc1 = self.conv_block(1, 64)
+        self.enc2 = self.conv_block(64, 128)
+        self.enc3 = self.conv_block(128, 256)
+        self.enc4 = self.conv_block(256, 512)
 
-    def encoder_block(self, input:torch.Tensor, in_channels:int, out_channels:int):
-        s = self.conv_block(input, in_channels, out_channels)
-        p = F.max_pool2d(s, 2)
+        self.bottleneck = self.conv_block(512, 1024)
 
-        return s, p
+        self.upconv4 = self.upconv(1024, 512)
+        self.dec4 = self.conv_block(1024, 512)
+        self.upconv3 = self.upconv(512, 256)
+        self.dec3 = self.conv_block(512, 256)
+        self.upconv2 = self.upconv(256, 128)
+        self.dec2 = self.conv_block(256, 128)
+        self.upconv1 = self.upconv(128, 64)
+        self.dec1 = self.conv_block(128, 64)
 
-    def decoder_block(self, input:torch.Tensor, skip_features:torch.Tensor, in_channels:int, out_channels:int):
-        d = self.upconv(input, in_channels, out_channels)
-        d = torch.cat((d, skip_features), dim=1)
-        d = self.conv_block(d, in_channels, out_channels)
+        self.outconv = nn.Conv2d(64, 1, kernel_size=1)
 
-        return d
+    def conv_block(self, in_channels, out_channels):
+        block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(0.25),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(0.25)
+        )
+        return block
+
+    def upconv(self, in_channels, out_channels):
+        return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
 
     def forward(self, x):
         # Down Sampling (Encoding)
-        s1, p1 = self.encoder_block(x, 1, 64)
-        s2, p2 = self.encoder_block(p1, 64, 128)
-        s3, p3 = self.encoder_block(p2, 128, 256)
-        s4, p4 = self.encoder_block(p3, 256, 512)
+        s1 = self.enc1(x)
+        p1 = F.max_pool2d(s1, 2)
+        s2 = self.enc2(p1)
+        p2 = F.max_pool2d(s2, 2)
+        s3 = self.enc3(p2)
+        p3 = F.max_pool2d(s3, 2)
+        s4 = self.enc4(p3)
+        p4 = F.max_pool2d(s4, 2)
 
         # Base(Bottleneck)
-        b1 = self.conv_block(p4, 512, 1024)
+        b1 = self.bottleneck(p4)
 
         # Up Sampling (Decoding)
-        d4 = self.decoder_block(b1, s4, 1024, 512)
-        d3 = self.decoder_block(d4, s3, 512, 256)
-        d2 = self.decoder_block(d3, s2, 256, 128)
-        d1 = self.decoder_block(d2, s1, 128, 64)
+        d4 = self.upconv4(b1)
+        d4 = torch.cat((d4, s4), dim=1)
+        d4 = self.dec4(d4)
+
+        d3 = self.upconv3(d4)
+        d3 = torch.cat((d3, s3), dim=1)
+        d3 = self.dec3(d3)
+
+        d2 = self.upconv2(d3)
+        d2 = torch.cat((d2, s2), dim=1)
+        d2 = self.dec2(d2)
+
+        d1 = self.upconv1(d2)
+        d1 = torch.cat((d1, s1), dim=1)
+        d1 = self.dec1(d1)
 
         # Output
-        out = nn.Conv2d(64, 1, kernel_size=1)(d1)
+        out = self.outconv(d1)
 
         return out
 
