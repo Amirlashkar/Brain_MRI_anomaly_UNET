37c782c0849006a9dd6593f4b9b2a803fdf30c4d
amireza1422 <amireza1422@gmail.com>
2024-07-23 17:59:57 +0330

    UNet added to components
    This kind of autoencoder is more efficient

--- components.py
+++ components.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset
 from constants import *
 
@@ -116,6 +117,58 @@ class AutoEncoder(nn.Module):
         x = self.bottleneck(x)
         x = self.decoder(x)
         return x
+
+
+class UNet(nn.Module):
+    def conv_block(self, input:torch.Tensor, in_channels:int, out_channels:int):
+        x = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)(input)
+        x = nn.BatchNorm2d(out_channels)(x)
+        x = nn.ReLU(inplace=True)(x)
+        x = nn.Dropout2d(.25)(x)
+        x = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)(x)
+        x = nn.BatchNorm2d(out_channels)(x)
+        x = nn.ReLU(inplace=True)(x)
+        x = nn.Dropout2d(.25)(x)
+
+        return x
+
+    def upconv(self, input:torch.Tensor, in_channels:int, out_channels:int):
+        up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)(input)
+        return up
+
+    def encoder_block(self, input:torch.Tensor, in_channels:int, out_channels:int):
+        s = self.conv_block(input, in_channels, out_channels)
+        p = F.max_pool2d(s, 2)
+
+        return s, p
+
+    def decoder_block(self, input:torch.Tensor, skip_features:torch.Tensor, in_channels:int, out_channels:int):
+        d = self.upconv(input, in_channels, out_channels)
+        d = torch.cat((d, skip_features), dim=1)
+        d = self.conv_block(d, in_channels, out_channels)
+
+        return d
+
+    def forward(self, x):
+        # Down Sampling (Encoding)
+        s1, p1 = self.encoder_block(x, 1, 64)
+        s2, p2 = self.encoder_block(p1, 64, 128)
+        s3, p3 = self.encoder_block(p2, 128, 256)
+        s4, p4 = self.encoder_block(p3, 256, 512)
+
+        # Base(Bottleneck)
+        b1 = self.conv_block(p4, 512, 1024)
+
+        # Up Sampling (Decoding)
+        d4 = self.decoder_block(b1, s4, 1024, 512)
+        d3 = self.decoder_block(d4, s3, 512, 256)
+        d2 = self.decoder_block(d3, s2, 256, 128)
+        d1 = self.decoder_block(d2, s1, 128, 64)
+
+        # Output
+        out = nn.Conv2d(64, 1, kernel_size=1)(d1)
+
+        return out
 
 if __name__ == "__main__":
     ae = AutoEncoder()
